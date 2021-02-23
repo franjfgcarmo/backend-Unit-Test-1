33f2743f7156b4b7b7221b0ee823635b202ff55e
Francisco José Fernandez García <francisco@smsdata.es>
2021-02-23 08:48:44 +0100

    feat: created first tests

---
+++ test/Tictactoe.Test/Models/TurnCompactTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tictactoe.Models;
+using Xunit;
+
+namespace Tictactoe.Test.Models
+{
+    public class TurnCompactTest
+    {
+        [Fact]
+        public void TestTurn()
+        {
+            var turn= new Turn();
+            Assert.Equal(Color.XS, turn.Take());
+        }
+        [Fact]
+        public void TestChange()
+        {
+            var turn = new Turn();
+            turn.Change();
+            Assert.Equal(Color.OS, turn.Take());
+            turn.Change();
+            Assert.Equal(Color.XS, turn.Take());
+            turn.Change();
+            Assert.Equal(Color.OS, turn.Take());
+        }
+    }
+}
---
+++ test/Tictactoe.Test/Models/TurnTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tictactoe.Models;
+using Xunit;
+
+namespace Tictactoe.Test.Models
+{
+    public class TurnTest
+    {
+        [Fact]
+        public void TurnTset()
+        {
+            //arrage
+            Turn turn;
+            //act
+            turn = new Turn();
+            var resultColor = turn.Take();
+            //assert
+            Color colorExpected = Color.XS;
+            Assert.Equal(colorExpected, resultColor);
+        }
+        #region [test is coupled to the code]
+        /*This test is coupled to the code, if tomorrow someone change the code of the test,
+         and doesn`t change the test name, can make us go crazy.
+        The TestChange method of the TurnCompactTest class does this three tests in one
+         */
+        [Fact]
+        public void TestOneChange()
+        {
+            //arrage
+            Turn turn = new Turn();
+            //act
+            turn.Change();
+            var resultColor = turn.Take();
+            //assert
+            Color colorExpected = Color.OS;
+            Assert.Equal(colorExpected, resultColor);
+        }
+        [Fact]
+        public void TestTwoChange()
+        {
+            //arrage
+            Turn turn = new Turn();
+            //act
+            turn.Change();
+            turn.Change();
+            var resultColor = turn.Take();
+            //assert
+            Color colorExpected = Color.XS;
+            Assert.Equal(colorExpected, resultColor);
+        }
+        [Fact]
+        public void TestThreeChange()
+        {
+            //arrage
+            Turn turn = new Turn();
+            //act
+            turn.Change();
+            turn.Change();
+            turn.Change();
+            var resultColor = turn.Take();
+            //assert
+            Color colorExpected = Color.OS;
+            Assert.Equal(colorExpected, resultColor);
+        }
+        #endregion
+
+
+    }
+}
