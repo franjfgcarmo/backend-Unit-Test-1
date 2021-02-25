82e5b69a383dac84687f8446c80883044a9d8c00
Francisco José Fernandez García <francisco@smsdata.es>
2021-02-25 22:24:32 +0100

    feat: differrent ways to do it

---
+++ test/Tictactoe.Test/Models/TurnWithAttributesTest .cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tictactoe.Models;
+using Xunit;
+
+namespace Tictactoe.Test.Models
+{
+    public class TurnWithAttributesTest
+    {
+        private Turn _turn;
+
+        private void Arrange() {
+            _turn = new Turn();
+        }
+
+        [Fact]
+        public void TestTurn()
+        {
+            Arrange();
+            Assert.Equal(Color.XS, _turn.Take());
+        }
+        [Fact]
+        public void TestChange()
+        {
+            Arrange();
+            _turn.Change();
+            Assert.Equal(Color.OS, _turn.Take());
+            _turn.Change();
+            Assert.Equal(Color.XS, _turn.Take());
+            _turn.Change();
+            Assert.Equal(Color.OS, _turn.Take());
+        }
+    }
+}
---
+++ test/Tictactoe.Test/Models/TurnWithConstructorTest.cs
@@ -0,0 +1,31 @@
+﻿using Tictactoe.Models;
+using Xunit;
+
+namespace Tictactoe.Test.Models
+{
+    public class TurnWithConstructorTest
+    {
+        private readonly Turn _turn;
+
+        public TurnWithConstructorTest()
+        {
+            _turn = new Turn();
+        }
+
+        [Fact]
+        public void TestTurn()
+        {
+            Assert.Equal(Color.XS, _turn.Take());
+        }
+        [Fact]
+        public void TestChange()
+        {
+            _turn.Change();
+            Assert.Equal(Color.OS, _turn.Take());
+            _turn.Change();
+            Assert.Equal(Color.XS, _turn.Take());
+            _turn.Change();
+            Assert.Equal(Color.OS, _turn.Take());
+        }
+    }
+}
