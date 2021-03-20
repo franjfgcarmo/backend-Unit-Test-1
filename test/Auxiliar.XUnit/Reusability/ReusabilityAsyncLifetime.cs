2f6177a781dabfff76fc8ae21b9ee45c7981cc5b
Francisco José Fernandez García <francisco@smsdata.es>
2021-03-20 13:49:36 +0100

    added Proof of concept with AsyncLifetime

---
+++ test/Auxiliar.XUnit/Reusability/ReusabilityAsyncLifetime.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Auxiliar.XUnit.Reusability
+{
+    public class ReusabilityAsyncLifetime : IAsyncLifetime
+    {
+        //https://mderriey.com/2017/09/04/async-lifetime-with-xunit/
+        //https://dev.to/tswiftma/initialize-asynchronous-data-in-xunit-collections-for-sharing-data-between-tests-39hc
+        private readonly ITestOutputHelper output;
+
+        public ReusabilityAsyncLifetime(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        public async Task DisposeAsync()
+        {
+            output.WriteLine($"{nameof(DisposeAsync)}");
+            await Task.CompletedTask;
+        }
+
+        public async Task InitializeAsync()
+        {
+            output.WriteLine($"{nameof(InitializeAsync)}");
+            await Task.CompletedTask;
+        }
+        [Fact]
+        public void Test1() {
+            output.WriteLine($"{nameof(Test1)}");
+        }
+        [Fact]
+        public void Test2() {
+            output.WriteLine($"{nameof(Test2)}");
+        }
+    }
+}
